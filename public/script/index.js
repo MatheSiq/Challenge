608a574dd658be82958fcf33597f02b4b8559cef
Matheus Siqueira <matheu.f.siqueira@gmail.com>
2024-08-22 02:42:19 -0300

    Paginas Atualizadas
    Foi realizado o  template da Home e da pagina Sobre o Projeto

---
+++ public/script/index.js
@@ -0,0 +1,25 @@
+document.getElementById('supportForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const Severidade = document.getElementById('Severidade').value;
+
+    let nivel = '';
+    switch(Severidade) {
+        case 'baixo':
+            nivel = '1';
+            break;
+        case 'medio':
+            nivel = '2';
+            break;
+        case 'alto':
+            nivel = '3';
+            break;
+    }
+
+    const mensagem = `O chamado foi analisado pela nossa inteligência artificial, e de acordo com a Severidade (${Severidade}) e o descritivo, o mesmo foi escalonado para um analista de Nível ${nivel}.`;
+
+    document.getElementById('modalMessage').textContent = mensagem;
+
+    const resultModal = new bootstrap.Modal(document.getElementById('resultModal'));
+    resultModal.show();
+});
